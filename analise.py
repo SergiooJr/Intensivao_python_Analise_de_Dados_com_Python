4c224910f9ea2a3b71ebe76b0e04499dc8813520
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-27 17:37:07 -0300

    Update analise.py

---
+++ analise.py
@@ -0,0 +1,23 @@
+'''Análise de Dados com Python
+    Desafio
+
+    Você trabalha em uma empresa de telecom e tem clientes de vários serviços diferentes entre os principais: internet e telefone.
+    
+    O problema é que analisando o histórico dos clientes dos últimos anos você percebeu que a empresa está com Churn de mais de 26% dos clientes.
+    Isso representa uma perda de milhões para a empresa.
+    O que a empresa precisa fazer para resolver isso?
+    Link Original do Kaggle: https://www.kaggle.com/radmirzosimov/telecom-users-dataset
+'''
+# Passo 1: Importar a base de dados
+
+# Passo 2: Visualizar a base de dados
+# Entender as informações que você tem disponível
+# Decobrir os problemas da base de dados
+
+# Passo 3: Tratamento de dados (resolver os problemas da base de dados)
+
+# Passo 4: Análise inicial dos dados
+# Confirmar se o que te foi passado está certo
+
+# Passo 5: Descobrir os motivos do cancelamento
+
